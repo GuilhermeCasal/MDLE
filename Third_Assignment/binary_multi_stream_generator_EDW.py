8415d551dc2bb42eab30d3a8b716bd4327b790d0
GuilhermeCasal <gcmartins@ua.pt>
2024-05-28 14:29:02 +0100

    final final

---
+++ Third_Assignment/binary_multi_stream_generator_EDW.py
@@ -0,0 +1,19 @@
+import time
+import random
+import json
+
+random.seed(57)
+
+class BinaryMultiStreamGenerator():
+  def __init__(self, num_streams):
+    super().__init__()
+    self.probs = [max(0.01, 0.5) for _ in range(num_streams)]
+
+  def generate(self, client_socket):
+    while True:
+        bits = [1 if random.random() < prob else 0 for prob in self.probs]
+        #data = str(bits) 
+        # send data as dict
+        data = json.dumps(dict([(i, b) for i, b in enumerate(bits)]))
+        client_socket.send((data + "\n").encode("utf-8"))
+        time.sleep(0.1)
